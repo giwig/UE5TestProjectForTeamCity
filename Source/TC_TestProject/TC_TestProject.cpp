9c38eab00bf96ac4a5dbae34376fb9ed6283cfeb
Ilya Hartmann <giwig2014@gmail.com>
2023-02-20 19:11:28 +0300

    Init

---
+++ Source/TC_TestProject/TC_TestProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TC_TestProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TC_TestProject, "TC_TestProject" );
---
+++ Source/TC_TestProject/TC_TestProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TC_TestProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TC_TESTPROJECT_API ATC_TestProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
